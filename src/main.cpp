2720f7d48dc0a952fdba3d7072ef3cbdd9f12756
Luigi Fusco <luigifusco98@gmail.com>
2022-06-07 15:32:28 +0200

    fix R button + sky

--- src/main.cpp
+++ src/main.cpp
@@ -316,16 +316,13 @@ private:
         if (glfwGetKey(window, GLFW_KEY_R)) {
             vertices.clear();
             indices.clear();
-            for (auto& chunk : chunkMap) {
-                delete chunk.second;
-            }
-            {
-                std::unique_lock l(mapM);
-                chunkMap.clear();
-            }
             isThreadStopped = true;
             inC.notify_one();
             chunkThread.join();
+            for (auto& chunk : chunkMap) {
+                delete chunk.second;
+            }
+            chunkMap.clear();
             Chunk::setSeed(rand());
             isThreadStopped = false;
             chunkThread = std::thread(
@@ -1680,7 +1677,7 @@ private:
         renderPassInfo.renderArea.extent = swapChainExtent;
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {{0.0f, 0.0f, 0.0f, 1.0f}};
+        clearValues[0].color = {{0.179f, 0.773f, 0.906f, 1.0f}};
         clearValues[1].depthStencil = {1.0f, 0};
 
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
